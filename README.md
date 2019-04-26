fe7d707798c06457d9b63e51f7452f192360655b
musmuris <mrpurplestreak@gmail.com>
2019-04-26 13:05:47 +0100

    Copy from actual code and sanitize

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Zuora-PDF-Generation
+# Zuora Quote PDF Generation
+
+Out of the box when generating a PDF Zuora Quotes can only make these as attachments onto an opportunity.  We had requirements this didn't meet:
+1. We wanted to attach the PDF to a Quote, not an opportunity
+2. We wanted public URLs to enable easy downloafing of PDFs without a salesforce login
+    1. You cannot make public facing URLs for attachments.
+    2. Attachments are being deprecated in favour of files in Salesforce anyway
+    3. Also files can be attached to many objects without duplication (originally we coppied the attachment to the quote from the opportunity)
+
+
+After adding comments to [this idea](https://community.zuora.com/t5/Zuora-CPQ-Ideas/Zuora-Quote-PDFs-and-Salesforce-Files/idc-p/27821#!%2F%23M1653) and seeing replies there we wrote the code here (sanitized to remove some company specific bits)
+
+There are 2 main parts to this which are a VisualForce page and controller to drive it, 
+and a class that does the actual heavy lifting. There are also some auxiliary classes for testing etc.
+And also a javascript button that we use to display the page in an iframe.
---
+++ create_quote_button.js
@@ -0,0 +1,48 @@
+var buttonScope = buttonScope || {}; 
+
+(function() {
+
+    function generateQuote(url) { 
+        var iframe = document.createElement("iframe"); 
+        iframe.src = url; 
+        iframe.style.height = "185px"; 
+        iframe.style.width = "100%"; 
+        iframe.style.marginTop = "15px";
+        iframe.style.display = "block"; 
+        iframe.style.border = "none"; 
+
+        var divFrame = document.getElementById("divForPdfFrame");
+        divFrame.appendChild(iframe);
+
+        // Listens to event from IFrame to reload quote page when done
+        window.addEventListener('message', function(event) {
+            if( event.data == 'pdfcomplete')
+                window.location.reload();
+        }, false);
+
+    }
+
+    var pageURL = window.location.hostname; 
+
+    function addDivForFrame() { 
+        var divForFrame = document.createElement('div'); 
+        divForFrame.id = "divForPdfFrame"; 
+        divForFrame.style.width = "30%"; 
+        divForFrame.style.height = "200px"; 
+        divForFrame.style.background = "white"; 
+        divForFrame.style.margin = "auto"; 
+        divForFrame.style.position = "fixed"; 
+        divForFrame.style.top = "25%"; 
+        divForFrame.style.left = "35%"; 
+        divForFrame.style.border = "thin solid black"; 
+        document.getElementsByTagName('body')[0].appendChild(divForFrame); 
+    } 
+
+    buttonScope.onClickGenerateQuotePdf = function() { 
+        addDivForFrame(); 
+        generateQuote('https://'+pageURL+'/apex/ZuoraGenerateQuotePdf?quoteId={!zqu__Quote__c.Id}'); 
+    } 
+
+})(); 
+
+buttonScope.onClickGenerateQuotePdf();
